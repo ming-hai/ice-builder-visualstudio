fabf27b066c2a3bc7fde904cf9b1dbe281aa4733
Jose <jose@zeroc.com>
2018-01-04 20:28:31 +0100

    Update version to 5.0.0

--- IceBuilder_VS2015/Properties/AssemblyInfo.cs
+++ IceBuilder_VS2015/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("ZeroC, Inc.")]
 [assembly: AssemblyProduct("Ice Builder")]
 [assembly: AssemblyCopyright("Copyright (c) 2009-2017 ZeroC, Inc.")]
-[assembly: AssemblyVersion("4.3.10")]
+[assembly: AssemblyVersion("5.0.0")]
 [assembly: ComVisibleAttribute(false)]
